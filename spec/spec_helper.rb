a141217b44afb09a24560ee899983ce7a8cf1943
Corey Powell <mistdragon100@gmail.com>
2015-05-01 11:20:15 -0500

    Added module test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,19 +10,27 @@ SimpleCov.start
 require 'moon-prototype/load'
 
 module Fixtures
+  module ExtensionPrototype
+    prototype_attr :exts
+  end
+
   class MyPrototypeObject
+    extend ExtensionPrototype
+
     class << self
       prototype_attr :thing
       prototype_attr :other_thing,  default: proc { Array.new }
       prototype_attr :map_of_thing, type: Hash
     end
 
+    exts << :main
     things << 'Thingy'
     other_things << 'Junk'
     map_of_things[:scrap] = 1
   end
 
   class MyPrototypeObjectSubClass < MyPrototypeObject
+    exts << :sub
     things << 'OtherThingy'
     other_things << 'SomeMoreJunk'
     map_of_things[:junk] = 1
